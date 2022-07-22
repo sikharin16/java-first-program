8445ee120e13ebc1b925038d771ba216e5694326
sikharin16 <sikharin.kadeeroj@accenture.com>
2022-07-22 16:30:54 +0700

    Finding the Best Loan Rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,13 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
+    }
+    public static int add(int[] number){
+        int sum = 0;
+        for(int item : number) {
+            sum+=item;
+        }
+        return  sum;
     }
 }
