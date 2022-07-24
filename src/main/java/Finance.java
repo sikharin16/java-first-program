65a7fa86b2113af9e5d5506ee6fc870d87d4ab56
sikharin16 <sikharin.kadeeroj@accenture.com>
2022-07-24 15:59:19 +0700

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,21 +1,22 @@
-package com.h2;
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator;
 
 import java.util.Arrays;
 import java.util.Map;
 
-
 public class Finance {
-    public static final  String BEST_LOAN_RATES  = "bestLoanRates";
-    public static final  String SAVINGS_CALCULATOR   = "savingsCalculator";
-    public static final  String MORTGAGE_CALCULATOR  = "mortgageCalculator";
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public static final   Map<String, String> commandsToUsage = Map.of(
+    public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
-    private static  boolean validateCommandArguments(String[] args){
+    private static boolean validateCommandArguments(String[] args) {
         switch (args[0]) {
             case BEST_LOAN_RATES:
                 return args.length == 1;
@@ -26,8 +27,8 @@ public class Finance {
         }
         return false;
     }
-    private static  void  executeCommand(String command ,String[] arguments ){
-        switch (command){
+    private static void executeCommand(String command, String[] arguments) {
+        switch (command) {
             case BEST_LOAN_RATES:
                 System.out.println("Finding best loan rates ...");
                 BestLoanRates.main(arguments);
@@ -41,23 +42,21 @@ public class Finance {
                 MortgageCalculator.main(arguments);
                 return;
         }
-
     }
 
     public static void main(String[] args) {
         String command = args[0];
-        if(!commandsToUsage.containsKey(command)){
+        if(!commandsToUsage.containsKey(command)) {
             System.out.println(command + ": command not found");
             return;
         }
+
         boolean isValidCommand = validateCommandArguments(args);
-        if(!isValidCommand){
+        if(!isValidCommand) {
             System.out.println(commandsToUsage.get(args[0]));
             return;
         }
-        executeCommand(command, Arrays.copyOfRange(args,1,args.length));
 
+        executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
-
-
 }
